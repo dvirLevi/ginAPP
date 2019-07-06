db2d7223034bd9d62ec74680993057f3c0626ecc
dvirLevi <dvirleviapp@gmail.com>
2019-07-07 00:37:32 +0300

    add

--- server.js
+++ server.js
@@ -58,38 +58,12 @@ app.post('/app/conect', async (req, res) => {
 })
 
 
-// cron.schedule("53 23 * * *", function() {
-//     var transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth: {
-//           user: 'dlibi312@gmail.com',
-//           pass: 'dlibi312312'
-//         }
-//       });
-      
-//       var mailOptions = {
-//         from: 'dlibi312@gmail.com',
-//         to: 'dvirlevi2@gmail.com',
-//         subject: 'Sending Email using Node.js',
-//         text: 'That was easy!'
-//       };
-      
-//       transporter.sendMail(mailOptions, function(error, info){
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.log('Email sent: ' + info.response);
-//         }
-//       });
-//   });
-
-  function dendmail(){
-  setInterval(()=>{
+cron.schedule("20 00 * * *", function() {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
           user: 'dlibi312@gmail.com',
-          pass: 'dlibi312312'
+          pass: '***'
         }
       });
       
@@ -107,10 +81,36 @@ app.post('/app/conect', async (req, res) => {
           console.log('Email sent: ' + info.response);
         }
       });
+  });
 
-  }, 60000 * 1);
-}
-dendmail()
+//   function dendmail(){
+//   setInterval(()=>{
+//     var transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//           user: 'dlibi312@gmail.com',
+//           pass: '***'
+//         }
+//       });
+      
+//       var mailOptions = {
+//         from: 'dlibi312@gmail.com',
+//         to: 'dvirlevi2@gmail.com',
+//         subject: 'Sending Email using Node.js',
+//         text: 'That was easy!'
+//       };
+      
+//       transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log('Email sent: ' + info.response);
+//         }
+//       });
+
+//   }, 60000 * 1);
+// }
+// dendmail()
 
 app.get('/app/:id', async (req, res) => {
     res.send(await collection.findOne({userId: req.params.id}))
