72f634cc121581a05ac75c3567ab013b33b15604
dvirLevi <dvirleviapp@gmail.com>
2019-07-09 10:23:31 +0300

    add

---
+++ client/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/',
+       'index.html',
+       'js/script.js',
+       'css/style.css',
+       'img/asset1.png',
+      //  '/pwa-examples/a2hs/images/fox2.jpg',
+      //  '/pwa-examples/a2hs/images/fox3.jpg',
+      //  '/pwa-examples/a2hs/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
